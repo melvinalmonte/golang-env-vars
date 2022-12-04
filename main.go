135dc1a6605f0f82dfaa14ebe8c816bcd094906c
Melvin A <melvinalmonte@gmail.com>
2022-12-04 12:51:57 -0500

    reformatted main

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "github.com/spf13/viper"
+	"fmt"
+	"github.com/spf13/viper"
 )
 
 func main() {
-    viper.SetConfigFile("./configs/dev.yaml")
-    viper.ReadInConfig()
+	viper.SetConfigFile("./configs/dev.yaml")
+	viper.ReadInConfig()
 
-    // add env variables as needed
-    val1 := viper.Get("KEY1").(string)
-    val2 := viper.Get("KEY2").(string)
+	// add env variables as needed
+	val1 := viper.Get("KEY1").(string)
+	val2 := viper.Get("KEY2").(string)
 	val3 := viper.Get("KEY3").(string)
 
-
-    fmt.Println(val1, val2, val3)
+	fmt.Println(val1, val2, val3)
 }

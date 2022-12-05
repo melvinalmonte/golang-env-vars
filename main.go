23e3dad3692380a8e99932166477bccd7e5db6e5
Melvin A <melvinalmonte@gmail.com>
2022-12-04 20:48:53 -0500

    formatting file

--- main.go
+++ main.go
@@ -9,38 +9,27 @@ func main() {
 	// viper.SetConfigFile("./configs/dev.yaml")
 	// viper.ReadInConfig()
 
-    viper.SetConfigName("dev") // config file name without extension
-    viper.SetConfigType("yaml") // config file extension
-    viper.AddConfigPath("./configs/")
-    viper.AutomaticEnv()
+	viper.SetConfigName("dev")  // config file name without extension
+	viper.SetConfigType("yaml") // config file extension
+	viper.AddConfigPath("./configs/")
+	viper.AutomaticEnv()
 
-    err := viper.ReadInConfig()
-    if err != nil{
-        fmt.Println("An error has occurred when reading config file: ", err)
-    }
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("An error has occurred when reading config file: ", err)
+	}
 
-    // Different ways to retrieve config values:
+	// Different ways to retrieve config values:
 
-    // Using .GetString
-    env := viper.GetString("app.env")
-    fmt.Println(env)
+	// Using .GetString
+	env := viper.GetString("app.env")
+	fmt.Println(env)
 
-    // Using Get, then declaring the type
-    token := viper.Get("app.token").(string)
-    fmt.Println(token)
+	// Using Get, then declaring the type
+	token := viper.Get("app.token").(string)
+	fmt.Println(token)
 
-    secret_data := viper.Get("app.secret_data").(string)
-    fmt.Println(secret_data)
+	secret_data := viper.Get("app.secret_data").(string)
+	fmt.Println(secret_data)
 
-
-
-
-
-
-	// // add env variables as needed
-	// val1 := viper.Get("KEY1").(string)
-	// val2 := viper.Get("KEY2").(string)
-	// val3 := viper.Get("KEY3").(string)
-
-	// fmt.Println(val1, val2, val3)
 }

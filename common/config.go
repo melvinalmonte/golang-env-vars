91736df3299336a451b9221566874a42124fc3d9
Melvin A <melvinalmonte@gmail.com>
2022-12-04 21:07:45 -0500

    wrote function to retrieve config values

---
+++ common/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+//Function to read an environment or return a default value
+func GetConfigValue(key string, defaultValue string) string {
+
+	viper.AddConfigPath("./configs/")
+	viper.SetConfigName("dev")  // config file name without extension
+	viper.SetConfigType("yaml") // config file extension
+	
+
+	//read configs and handle errors
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("An error has occurred when reading config file: ", err)
+	}
+	
+    value := viper.Get(key).(string)
+	if value != "" {
+		return value
+	}
+	return defaultValue
+}
--- main.go
+++ main.go
@@ -2,34 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
+	config "golang-configs/common"
 )
 
 func main() {
-	// viper.SetConfigFile("./configs/dev.yaml")
-	// viper.ReadInConfig()
 
-	viper.SetConfigName("dev")  // config file name without extension
-	viper.SetConfigType("yaml") // config file extension
-	viper.AddConfigPath("./configs/")
-	viper.AutomaticEnv()
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		fmt.Println("An error has occurred when reading config file: ", err)
-	}
-
-	// Different ways to retrieve config values:
-
-	// Using .GetString
-	env := viper.GetString("app.env")
+	env := config.GetConfigValue("app.env", "")
 	fmt.Println(env)
 
 	// Using Get, then declaring the type
-	token := viper.Get("app.token").(string)
+	token := config.GetConfigValue("app.token", "")
 	fmt.Println(token)
 
-	secret_data := viper.Get("app.secret_data").(string)
+	secret_data := config.GetConfigValue("app.secret_data", "")
 	fmt.Println(secret_data)
 
 }
